cbf88b7212b4a0ee75f3c2c84bc52c8236227014
Aleksandr Mineev <aleksandr.mineev@jetbrains.com>
2025-03-20 17:38:15 +0200

    test

--- src/app/sidepanel/new.tsx
+++ src/app/sidepanel/new.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-// import { signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { CLOUD_PROJECT_NUMBER, MAIN_STAGE_URL } from "@/constants";
+import { useEffect, useState } from "react";
+import { meet, MeetSidePanelClient } from "@googleworkspace/meet-addons";
 
 export default function Home() {
-  // const { data: session, status } = useSession();
+  const [sidePanelClient, setSidePanelClient] = useState<MeetSidePanelClient>();
+
+  const { data: session, status } = useSession();
 
   const popupCenter = (url: string, title: string) => {
     const dualScreenLeft = window.screenLeft ?? window.screenX;
@@ -33,30 +38,53 @@ export default function Home() {
     newWindow?.focus();
   };
 
-  // if (status === "authenticated") {
-  //   return (
-  //     <div>
-  //       <h2> Welcome {session.user?.email} 😀</h2>
-  //
-  //       <pre>{JSON.stringify(session, null, 2)}</pre>
-  //
-  //       <button onClick={() => signOut()}>Sign out</button>
-  //     </div>
-  //   );
-  // }
-  // } else if (status === "unauthenticated") {
+  // Launches the main stage when the main button is clicked.
+  async function startActivity() {
+    if (!sidePanelClient) {
+      throw new Error("Side Panel is not yet initialized!");
+    }
+    await sidePanelClient.startActivity({
+      mainStageUrl: MAIN_STAGE_URL,
+    });
+  }
+
+  /**
+   * Prepares the add-on Side Panel Client.
+   */
+  useEffect(() => {
+    (async () => {
+      const session = await meet.addon.createAddonSession({
+        cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+      });
+
+      setSidePanelClient(await session.createSidePanelClient());
+    })();
+  }, []);
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <h2> Welcome {session.user?.email} 😀</h2>
+
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  } else if (status === "unauthenticated") {
+    return (
+      <div>
+        <h2>Please Login</h2>
+        <button onClick={() => popupCenter("/google-signin", "Sample Sign In")}>
+          Sign In with Google
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h2>Please Login</h2>
-      <button onClick={() => popupCenter("/google-signin", "Sample Sign In")}>
-        Sign In with Google
-      </button>
+      <h1>Loading...</h1>
     </div>
   );
-
-  // return (
-  //     <div>
-  //         <h1>Loading...</h1>
-  //     </div>
-  // );
 }
