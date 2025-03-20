ccabea1cd22a39138af7fba031c067bf9a36cd33
Aleksandr Mineev <aleksandr.mineev@jetbrains.com>
2025-03-20 17:35:44 +0200

    test

--- src/app/sidepanel/new.tsx
+++ src/app/sidepanel/new.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
+// import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  // const { data: session, status } = useSession();
 
   const popupCenter = (url: string, title: string) => {
     const dualScreenLeft = window.screenLeft ?? window.screenX;
@@ -33,17 +33,17 @@ export default function Home() {
     newWindow?.focus();
   };
 
-  if (status === "authenticated") {
-    return (
-      <div>
-        <h2> Welcome {session.user?.email} 😀</h2>
-
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
-    );
-  }
+  // if (status === "authenticated") {
+  //   return (
+  //     <div>
+  //       <h2> Welcome {session.user?.email} 😀</h2>
+  //
+  //       <pre>{JSON.stringify(session, null, 2)}</pre>
+  //
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </div>
+  //   );
+  // }
   // } else if (status === "unauthenticated") {
   return (
     <div>
