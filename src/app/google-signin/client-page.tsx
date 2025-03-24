5a9d817def9190ea55329bb1fb5510ed532a6613
Aleksandr Mineev <aleksandr.mineev@jetbrains.com>
2025-03-24 09:29:01 +0200

    one more attempt

--- src/app/google-signin/client-page.tsx
+++ src/app/google-signin/client-page.tsx
@@ -1,28 +1,31 @@
-'use client'
+"use client";
 
 import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 const ClientSignInPage = () => {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    useEffect(() => {
-        if (!(status === "loading") && !session) void signIn("google");
-        if (session) window.close();
-    }, [session, status]);
+  useEffect(() => {
+    debugger;
 
-    return (
-        <div
-            style={{
-                width: "100vw",
-                height: "100vh",
-                position: "absolute",
-                left: 0,
-                top: 0,
-                background: "white",
-            }}
-        ></div>
-    );
+    if (!(status === "loading") && !session) void signIn("google");
+
+    if (session) window.close();
+  }, [session, status]);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        background: "white",
+      }}
+    ></div>
+  );
 };
 
 export default ClientSignInPage;
