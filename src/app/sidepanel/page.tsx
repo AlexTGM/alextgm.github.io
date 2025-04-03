6b80d071cf61fa3a1d9f1885e9e2c49d5123e690
Aleksandr Mineev <aleksandr.mineev@jetbrains.com>
2025-04-03 14:43:47 +0300

    one more try

--- src/app/sidepanel/page.tsx
+++ src/app/sidepanel/page.tsx
@@ -1,33 +1,31 @@
 'use server'
 
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
 import ClientPage from "@/app/sidepanel/client-page";
 import { SignOut } from "@/app/sidepanel/sign-out";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const cookie = cookieStore.get("google_jwt");
-
-  if (!cookie?.value) {
-    return <div>Unauthorized</div>;
-  }
-
-  const response = await (
-    await fetch(
-      "https://services.reops.labs.jb.gg/drive-watch/ext/projects/sessions",
-      {
-        headers: { "Google-Id-Token": cookie?.value },
-      },
-    )
-  ).json();
+  // const cookieStore = await cookies();
+  // const cookie = cookieStore.get("google_jwt");
+  //
+  // if (!cookie?.value) {
+  //   return <div>Unauthorized</div>;
+  // }
+  //
+  // const response = await (
+  //   await fetch(
+  //     "https://services.reops.labs.jb.gg/drive-watch/ext/projects/sessions",
+  //     {
+  //       headers: { "Google-Id-Token": cookie?.value },
+  //     },
+  //   )
+  // ).json();
 
   return (
     <div>
-      <pre>{JSON.stringify(response, null, 2)}</pre>
-
+      {/*<pre>{JSON.stringify(response, null, 2)}</pre>*/}
       <SignOut />
-
       <ClientPage />
     </div>
   );
